1368175402ede90f7ffb7e4cf5f6a7c6db812d1d
Vanessa Su <vnessa.su@gmail.com>
2021-05-14 18:04:29 -0400

    :hammer: Add routes for displaying and adding vendors

--- VendorOrderTracker/Controllers/VendorsController.cs
+++ VendorOrderTracker/Controllers/VendorsController.cs
@@ -1,9 +1,29 @@
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using VendorOrderTracker.Models;
 
 namespace VendorOrderTracker.Controllers
 {
   public class VendorsController : Controller
   {
-    
+    [HttpGet("/vendors")]
+    public ActionResult Index()
+    {
+      Dictionary<int, Vendor> allVendors = Vendor.GetAllVendors();
+      return View(allVendors);
+    }
+
+    [HttpGet("/vendors/new")]
+    public ActionResult New()
+    {
+      return View();
+    }
+
+    [HttpPost("/vendors")]
+    public ActionResult Create(string vendorName, string vendorDescription, string vendorAddress, string vendorPhoneNumber)
+    {
+      Vendor newVendor = new Vendor(vendorName, vendorDescription, vendorAddress, vendorPhoneNumber);
+      return RedirectToAction("Index", "Home");
+    }
   }
 }
