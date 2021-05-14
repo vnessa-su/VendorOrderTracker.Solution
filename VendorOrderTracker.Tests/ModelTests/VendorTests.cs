0ecc02cbe926d0766a1fa1a14cff76b2e3065ecc
Vanessa Su <vnessa.su@gmail.com>
2021-05-14 13:06:08 -0400

    :hammer: Add interface and Dispose method

--- VendorOrderTracker.Tests/ModelTests/VendorTests.cs
+++ VendorOrderTracker.Tests/ModelTests/VendorTests.cs
@@ -1,16 +1,22 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using VendorOrderTracker.Models;
 
 namespace VendorOrderTracker.Tests
 {
   [TestClass]
-  public class VendorTests
+  public class VendorTests : IDisposable
   {
     private static string _vendorName = "Explorateur Cafe";
     private static string _description = "Californian French cafe, bar and restaurant";
     private static string _address = "186 Tremont St, Boston, MA 02116";
     private static string _phoneNumber = "617-466-6600";
     private Vendor _vendorObject = new Vendor(_vendorName, _description, _address, _phoneNumber);
+
+    public void Dispose()
+    {
+      _vendorObject.ClearOrders();
+    }
 
     [TestMethod]
     public void AddOrder_OrderObject_ListWithOneOrder()
@@ -22,7 +28,6 @@ namespace VendorOrderTracker.Tests
       decimal price = 20.00m;
       Order newOrder = new Order(orderTitle, orderDescription, orderDate, deliveryDate, price);
       _vendorObject.AddOrder(newOrder);
-      Assert.AreEqual(1, )
     }
   }
 }
