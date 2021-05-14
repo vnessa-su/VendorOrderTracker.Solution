3fa3810ff77fb1194b41274bb0d82138783cc4fc
Vanessa Su <vnessa.su@gmail.com>
2021-05-14 12:42:45 -0400

    :construction: WIP: Vendor tests for AddOrder

--- VendorOrderTracker.Tests/ModelTests/VendorTests.cs
+++ VendorOrderTracker.Tests/ModelTests/VendorTests.cs
@@ -1,1 +1,28 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VendorOrderTracker.Models;
+
+namespace VendorOrderTracker.Tests
+{
+  [TestClass]
+  public class VendorTests
+  {
+    private static string _vendorName = "Explorateur Cafe";
+    private static string _description = "Californian French cafe, bar and restaurant";
+    private static string _address = "186 Tremont St, Boston, MA 02116";
+    private static string _phoneNumber = "617-466-6600";
+    private Vendor _vendorObject = new Vendor(_vendorName, _description, _address, _phoneNumber);
+
+    [TestMethod]
+    public void AddOrder_OrderObject_ListWithOneOrder()
+    {
+      string orderTitle = "Regular Croissant Order";
+      string orderDescription = "20 Croissants - $1.00 each";
+      string orderDate = "05/04/2021";
+      string deliveryDate = "05/16/2021";
+      decimal price = 20.00m;
+      Order newOrder = new Order(orderTitle, orderDescription, orderDate, deliveryDate, price);
+      _vendorObject.AddOrder(newOrder);
+      Assert.AreEqual(1, )
+    }
+  }
+}
---
+++ VendorOrderTracker/Models/Vendor.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+
+namespace VendorOrderTracker.Models
+{
+  public class Vendor
+  {
+    public string Name {get; set;}
+    public string Description {get; set;}
+    public string Address {get; set;}
+    public string PhoneNumber {get; set;}
+    public static int Id {get; private set;}
+
+    private List<Order> _vendorOrders =  new List<Order>{};
+
+    public Vendor(string name, string description, string address, string phoneNumber)
+    {
+      Name = name;
+      Description = description;
+      Address = address;
+      PhoneNumber = phoneNumber;
+      Id = 0;
+    }
+
+    public void AddOrder(Order newOrder)
+    {
+
+    }
+  }
+}
